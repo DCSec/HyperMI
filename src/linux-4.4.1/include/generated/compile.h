b766fa89d302e6f25339a4528412af5278dbe7ad
kunlynn <linkunli@iie.ac.cn>
2019-09-17 20:05:12 +0800

    add modified linux kernel

---
+++ src/linux-4.4.1/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 6 */
+/* SMP */
+#define UTS_MACHINE "x86_64"
+#define UTS_VERSION "#6 SMP Mon Jan 7 21:08:01 CST 2019"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "ubuntu"
+#define LINUX_COMPILER "gcc version 4.8.4 (Ubuntu 4.8.4-2ubuntu1~14.04.4) "
